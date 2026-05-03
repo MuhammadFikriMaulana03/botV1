8b7c5c4c33faeb76e220d5fb6d30742992144c0b
MuhammadFikriMaulana03 <75489580+MuhammadFikriMaulana03@users.noreply.github.com>
2026-05-03 21:58:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ executor = ThreadPoolExecutor(max_workers=5)
 
 warnings.filterwarnings("ignore")
 
-TOKEN = os.getenv("8746301929:AAGJmL-MOMNqT1VG5Jmv5GZ_d6cFuOPba4s")
+TOKEN = "8746301929:AAGJmL-MOMNqT1VG5Jmv5GZ_d6cFuOPba4s"
 
 
 CACHE = {}
